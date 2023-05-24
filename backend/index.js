de58dc15eb3ed39eb241658e26ab18e813337c46
nikosetiawanp <nikosetiawanp@gmail.com>
2023-05-24 11:50:56 +0700

    fixed CORS error when sending GET request form client

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 express.json();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use("/", routes);
 app.listen(port, () => {
   console.log(`Server Started at ${port}`);
